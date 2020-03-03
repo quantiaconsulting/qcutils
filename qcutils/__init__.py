3181ff9d13e4a0ceb8066dbaafe334a53330d15f
Marco Balduini <balduini.marco@gmail.com>
2020-03-03 16:45:12 +0100

    added yaml config parsing for the aws credential updated the folder tree

--- qcutils/__init__.py
+++ qcutils/__init__.py
@@ -1,6 +1,16 @@
-def init_session(spark_session):
+import yaml
+
+def init_session(spark_session, cf_path = "/home/jovyan/utils/config.yaml"):
+
+    with open(cf_path, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    
+    aws_key = cfg['aws']['access']['key']
+    aws_secret = cfg['aws']['access']['secret']
+    
     hadoop_conf = spark_session.sparkContext._jsc.hadoopConfiguration()
-    hadoop_conf.set("fs.s3a.access.key", "AKIAXH7JU6RNMFM23HUM")
-    hadoop_conf.set("fs.s3a.secret.key", "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")
+    hadoop_conf.set("fs.s3a.access.key", aws_key)
+    hadoop_conf.set("fs.s3a.secret.key", aws_secret)
     hadoop_conf.set("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    
     return
