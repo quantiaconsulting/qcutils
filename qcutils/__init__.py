8a3d3d7328aabe702eae8919fb213f5e6e32d6a3
Marco Balduini <balduini.marco@gmail.com>
2020-03-13 15:18:09 +0100

    minor bug fix

--- qcutils/__init__.py
+++ qcutils/__init__.py
@@ -71,7 +71,7 @@ def list_s3_bucket_objects(bucket_name='quantia-master', prefix='training', limi
     for obj in objects.get('Contents')[:limit]:
         print(obj.get('Key')) 
 
-def print_s3_bucket_object(bucket_name='quantia-master', key, size=1000):
+def print_s3_bucket_object(key, bucket_name='quantia-master', size=1000):
     obj = s3.get_object(
         Bucket=bucket_name, 
         Key=key)
