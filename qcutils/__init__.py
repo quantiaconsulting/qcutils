f68240ef7fec53470268015ab9b2d0c15b506cac
Marco Balduini <balduini.marco@gmail.com>
2020-03-13 15:19:29 +0100

    bug fixes

--- qcutils/__init__.py
+++ qcutils/__init__.py
@@ -66,12 +66,14 @@ def create_kafka_topic(conf, topic, partitions=4,replication=3):
                 print("{}".format(e))
 
 def list_s3_bucket_objects(bucket_name='quantia-master', prefix='training', limit=10):
+    s3 = boto3.client('s3')
     objects = s3.list_objects_v2(Bucket=bucket_name, Prefix =prefix)
 
     for obj in objects.get('Contents')[:limit]:
         print(obj.get('Key')) 
 
 def print_s3_bucket_object(key, bucket_name='quantia-master', size=1000):
+    s3 = boto3.client('s3')
     obj = s3.get_object(
         Bucket=bucket_name, 
         Key=key)
