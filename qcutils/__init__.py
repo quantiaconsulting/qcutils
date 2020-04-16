de1406f33dafebd40f5c7baaaae3b9adcdd7cddf
Marco Balduini <balduini.marco@gmail.com>
2020-04-16 11:19:25 +0200

    Bug fixes

--- qcutils/__init__.py
+++ qcutils/__init__.py
@@ -19,26 +19,6 @@ def read_config_value(key,cf_path = "/home/jovyan/materials/utils/config.yaml"):
 
     return value
 
-def init_session(spark_session, cf_path = "/home/jovyan/materials/utils/config.yaml"):
-
-    with open(cf_path, 'r') as ymlfile:
-        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-    
-    #Read the AWS key and secret from cofiguration file
-    aws_key = cfg['aws']['access']['key']
-    aws_secret = cfg['aws']['access']['secret']
-    
-    #Set-up the hadoop configuration to enable s3a filesystem
-    hadoop_conf = spark_session.sparkContext._jsc.hadoopConfiguration()
-    hadoop_conf.set("fs.s3a.access.key", aws_key)
-    hadoop_conf.set("fs.s3a.secret.key", aws_secret)
-    hadoop_conf.set("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-
-    #Enable better dataframe visualiztion in jupyter
-    spark_session.conf.set("spark.sql.repl.eagerEval.enabled", True)
-    
-    return
-
 def init_spark_session(spark_session, cf_path = "/home/jovyan/materials/utils/config.yaml"):
 
     with open(cf_path, 'r') as ymlfile:
